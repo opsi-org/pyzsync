13ab17f3ba0931e8fb75f8529f3ee61b64a86662
Jan Schneider <j.schneider@uib.de>
2023-05-30 12:55:14 +0200

    [chg] Change limit

--- tests/test_zsync.py
+++ tests/test_zsync.py
@@ -327,7 +327,7 @@ def test_create_zsync_file(tmp_path: Path) -> None:
 		("random", 2048, 1, 66, 40),
 		("random", 2048, 2, 5, 1.1),
 		("random", 2048, 3, 2, 1.001),
-		("random", 2048, 4, 1, 1.00011),
+		("random", 2048, 4, 2, 1.00011),
 		("random", 4096, 1, 66, 40),
 		("random", 4096, 2, 4, 1.1),
 		("random", 4096, 3, 2, 1.001),
